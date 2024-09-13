0e26ee86ae3a9b3f5eb7cf6b431f204f5cc95e0e
omkargarde <42486951+omkargarde@users.noreply.github.com>
2024-09-13 15:55:27 +0530

    mongo working

--- server/server.js
+++ server/server.js
@@ -1,21 +1,30 @@
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import express from "express";
-import mongoose from "mongoose";
-mongoose
-  .connect(
-    "mongodb+srv://omyagarde:mongomern@cluster0.tktzq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  )
-  .then(() => {
-    console.log("mongoDB connected");
-  })
-  .catch((error) => console.error(error));
-const app = express();
-const PORT = process.env.PORT || 5000;
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
 
+const uri =
+  "mongodb+srv://omyagarde:superman123@cluster0.abxc8yr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const app = express();
+const PORT = process.env.PORT || 6000;
+mongoose
+  .connect(uri, {
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000,
+  })
+  .then(() => console.log("Connected successfully to MongoDB"))
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB", err);
+    if (err.name === "MongoTimeoutError") {
+      console.log(
+        "Timeout error. Please check your network connection and MongoDB Atlas status."
+      );
+    }
+  });
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173/",
+    origin: "http://localhost:5173/",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
